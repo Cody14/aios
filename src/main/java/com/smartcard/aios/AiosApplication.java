947b4f05c649904445a5060e4cfcad0527ca975c
Gasana14 <55234881+Gasana14@users.noreply.github.com>
2020-08-15 11:21:25 +0200

    initial commit

--- src/main/java/com/smartcard/aios/AiosApplication.java
+++ src/main/java/com/smartcard/aios/AiosApplication.java
@@ -2,8 +2,12 @@ package com.smartcard.aios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.smartcard.aios")
+@EntityScan("com.smartcard.aios.models")
 public class AiosApplication {
 
 	public static void main(String[] args) {
